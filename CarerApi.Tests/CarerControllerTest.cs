e032e22c5d3a53e2fd70cac33f98b8474c5ba784
Lewis Prescott <lewis.prescott@ceracare.co.uk>
2021-07-05 11:07:04 +0100

    revert commits

--- CarerApi.Tests/CarerControllerTest.cs
+++ CarerApi.Tests/CarerControllerTest.cs
@@ -26,7 +26,6 @@ namespace CarerApi.Tests
             HttpResponseMessage response;
             try
             {
-              
                 response = await _client.GetAsync("/Carer/2021-5-15T00:00:00");
                 Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
             }
@@ -39,33 +38,10 @@ namespace CarerApi.Tests
             Assert.AreEqual(true, cp.workingDay);
         }
 
-      
-         [Test]
-           public async Task NegativeTest()
-           {
-           
-            HttpResponseMessage response;
-            try
-            {
-              
-                response = await _client.GetAsync("/Carer/2021-6-13T00:00:00");
-                int code = (int)response.StatusCode;
-                if(code == 204)
-                {
-                    console.log("It is not working day!);
-                }
-                
-            }
-            catch
-            {
-                throw new Exception("Status Code not 204");
-            }
-        }
-           
-           
-           
-           }
-        
+        /** Add a test for negative scenario
+         * [Test]
+           public async Task NegativeTest(){}
+         */
 
         [OneTimeTearDown]
         public void TearDown()
